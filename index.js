cc048d87c1d332f73a35df6e8f2a48b69e1ba056
sergp <sergeyputyrskiy@gmail.com>
2018-03-11 22:00:04 +0200

    Added route for signup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const signupRoutes = require('./routes/signup');
 
 app.get('/', (req, res) => res.send('Hello world!'));
+app.use('/signup', signupRoutes);
 
 app.listen(3000, () => console.log('App listen 3000 port'));
