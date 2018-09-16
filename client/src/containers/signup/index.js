e81520ed605fd568d3e663b5eb2566460c9f71a7
sergp <sergeyputyrskiy@gmail.com>
2018-09-16 22:43:13 +0300

    Test signup request from client

--- client/src/containers/signup/index.js
+++ client/src/containers/signup/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 class Signup extends React.PureComponent {
+  state = {
+    userName: 'serg',
+    firstName: 'Sergey',
+    lastName: 'Putyrskiy',
+    email: 'serg@mail.com',
+    password: '1234',
+  };
+
+  handleSubmit = () => {
+    console.log(this.state);
+    axios.post('http://localhost:3000/signup', this.state);
+  }
+
   render() {
     return (
       <div>
@@ -24,6 +38,7 @@ class Signup extends React.PureComponent {
           Password:
         </label>
         <input type="password" name="" id="password" />
+        <button onClick={this.handleSubmit}>Sign Up</button>
       </div>
     );
   }
